5acbf93f0f07c6e284944225a87d21431ae767d2
Richard.He <1959800587@qq.com>
2020-10-15 15:32:27 +0800

    添加项目文件。

---
+++ OracleDbHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OracleDbHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
